937eb69bd68d42e83c2e6e43f9ea4abb49b9bc60
snoutibur <157987813+snoutibur@users.noreply.github.com>
2025-06-06 16:23:02 -0600

    Taking user input for vectors

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,74 @@
 use std::fs::File;
 use std::vec::Vec;
-use std::io::{BufRead, BufReader};
+use std::io::{BufRead, BufReader, Write};
 
-/* -------------------------------------- */
-/* Wordle for dirty cheaters!             */
-/* -------------------------------------- */
+use rustyline;
 
 fn main() {
     println!("Hai you dirty cheater!!");
-    
+
     let word_bank: Vec<String> = load_words("word_bank.txt");
     let possible_words: Vec<String>;
 
-    let not: Vec<char>; // Letters not in the word
-    let possible: Vec<char>; // Letters that are in the word
-    let correct: [char;5] = [' ', ' ', ' ', ' ', ' ']; // Letters in the correct place
-    
+    let mut not: Vec<char> = Vec::new(); // Letters not in the word
+    let mut possible: Vec<char> = Vec::new(); // Letters that are in the word
+    let mut correct: [char;5] = [' ', ' ', ' ', ' ', ' ']; // Letters in the correct place
+
+    let mut rl = rustyline::DefaultEditor::new().unwrap();
+
+    /* -------------------------------------- */
+    /* Wordle for dirty cheaters!             */
+    /* -------------------------------------- */
+
+    loop {
+        println!();
+
+        // Display progress //
+        print!("Correct: ");
+        for(i, c) in correct.iter().enumerate() {
+            if *c == ' ' {
+                print!("- "); // Empty spaces
+            } else {
+                print!("{}", c);
+            }
+        }
+        println!();
+
+        println!("Invalid: {}", not.iter().collect::<String>());
+        println!("Possible: {}", possible.iter().collect::<String>());
+
+
+        // Main menu //
+        println!("------- DATA ENTRY! -------");
+        println!("1) NOT in word");
+        println!("2) POSSIBLE letters");
+        println!("3) CORRECT position");
+
+        let choice = match rl.readline(">> ") {
+            Ok(line) => line,
+            Err(_) => {
+                println!("Error reading line!");
+                continue;
+            }
+        };
+
+        match choice.as_str() {
+            "1" => {
+                println!("Enter characters NOT in the word:");
+                input_chars(&mut not);
+            }
+            "2" => {
+                println!("Enter characters POSSIBLE in the word:");
+                input_chars(&mut possible);
+            }
+            "3" => {
+                println!("Enter the CORRECT positions of letters");
+                
+            }
+            _ => println!("NUH UH !!")
+        }
+
+    }
 }
 
 fn load_words(file: &str) -> Vec<String> {
@@ -35,4 +88,20 @@ fn load_words(file: &str) -> Vec<String> {
 
     println!("{} words loaded!", words.len());
     return words;
+}
+
+fn input_chars(list: &mut Vec<char>) {
+    // Taking input
+    let mut rl = rustyline::DefaultEditor::new().unwrap();
+    let user_input = match rl.readline(">> ") {
+        Ok(line) => line,
+        Err(_) => {
+            "Line can't be read".to_string()
+        }
+    };
+
+    // Parse into chars and push to list
+    for char in user_input.chars() {
+        list.push(char);
+    }
 }
